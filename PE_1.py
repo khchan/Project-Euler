1933d1a23ac61f3da1882538f1771dd1a5272ab4
Kevin Chan <kvii.chan@gmail.com>
2012-03-26 23:06:07 -0400

    PE sol 1

--- PE_1.py
+++ PE_1.py
@@ -1,6 +1,6 @@
-def multiples_of_3_or_5():
-    for number in xrange(1000):
-        if not number % 3 or not number % 5:
-            yield number
+def mult_3or5():
+    for num in xrange(1000):
+        if not num % 3 or not num % 5:
+            yield num
 
-print sum(multiples_of_3_or_5())
+print sum(mult_3or5())
